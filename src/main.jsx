7bae7de08a44e9728e45c35bcd15989fbbd9bfff
kevin-contreras <kevin.contreras@itprofis-gt.com>
2023-06-18 03:20:47 -0600

    sexto Commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,15 @@ import Principal from './principal'
 import Login from './components/login'
 import Nav from './components/nav'
 import './index.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, Router } from "react-router-dom";
 import About from './components/about'
 import CartVista from './components/carritoVista'
 import Spotify from './components/spotify'
 import Usuario from './components/user'
 
 function App (){
-  const navigate = useNavigate();
 
-  // Función para redirigir a la ruta /spotify
-  const redirectToSpotify = () => {
-    navigate('/spotify');
-  };
+
 
   return (
     <BrowserRouter>
@@ -24,8 +20,7 @@ function App (){
         <Route path="/" element={<Spotify />} />
         <Route path="/spotify" element={<Principal />} />
         <Route path="/user" element={<Usuario />} />
-        {/* Redirigir a /spotify si ninguna ruta coincide */}
-        <Route path="*" element={redirectToSpotify} />
+        <Route path="*" element={<Principal />} />
       </Routes>
     </BrowserRouter>
   );
